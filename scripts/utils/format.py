ef14fa745e1080da425a601792586b7728f21b9b
pmeletis <p_c_meletis@yahoo.com>
2020-04-07 12:34:48 +0200

    Add unity tests for decode_uids and fix bugs according to tests. Documentation is also improved.

--- scripts/utils/format.py
+++ scripts/utils/format.py
@@ -20,6 +20,12 @@ def decode_uids(uids,
   the semantic ids (sids), instance ids (iids), and part ids (pids).
   Optionally (experimental for now), returns the sids_iids and sids_pids as well.
 
+  sids_iids represent the semantic-instance-level (two-level) labeling,
+  e.g., sids_iids(Cityscapes-Panoptic-Parts) = Cityscapes-Original.
+
+  sids_pids = sids * 100 + max(pids, 0) and represent the semantic-part-level labeling,
+  e.g., sids_pids(23_003_04) = 23_04.
+
   Args:
     uids: tf.Tensor of dtype tf.int32 and arbitrary shape,
           or tf.ndarray of dtype np.int32 and arbitrary shape,
@@ -29,13 +35,15 @@ def decode_uids(uids,
 
   Return:
     sids, iids, pids: same type and shape as uids, with -1 for not relevant pixels.
-        sids will have no -1.
-        iids will have -1 for pixels labeled with sids only.
-        pids will have -1 for pixels labeled with sids or sids_iids only.
+  
+    sids will have no -1.
+    iids will have -1 for pixels labeled with sids only.
+    pids will have -1 for pixels labeled with sids or sids_iids only.
+  
     if experimental_return_sids_iids:
-      sids_iids: same type and shape as uids, with -1 for not relevant pixels.
+      sids_iids: same type and shape as uids, will have no -1.
     if experimental_return_sids_pids:
-      sids_pids: same type and shape as uids, with -1 for not relevant pixels.
+      sids_pids: same type and shape as uids, will have no -1.
   """
 
   if isinstance(uids, np.ndarray):
@@ -45,6 +53,7 @@ def decode_uids(uids,
     ones_like = np.ones_like
     divmod_ = np.divmod
     maximum = np.maximum
+    dtype = np.int32
   elif isinstance(uids, tf.Tensor):
     if uids.dtype != tf.int32:
       raise TypeError(f'{uids.dtype} is an unsupported dtype of tf.Tensor uids.')
@@ -52,20 +61,34 @@ def decode_uids(uids,
     ones_like = tf.ones_like
     divmod_ = lambda x, y: (x // y, x % y)
     maximum = tf.maximum
+    dtype = lambda x: x
   elif isinstance(uids, (int, np.int32)):
     where = lambda cond, true_br, false_br: true_br if cond else false_br
     ones_like = lambda the_like: type(the_like)(1)
     divmod_ = divmod
     maximum = max
+    dtype = (lambda x: x) if isinstance(uids, int) else np.int32
   else:
     raise TypeError(f'{type(uids)} is an unsupported type of uids.')
 
+  # TODO(panos): find better solution, e.g. convert to necessary type only before returning?
+  # explanations for using dtype, np.asarray that should be not needed in the following lines
+  # dtype: numpy implicitly converts Python int literals in np.int64, we need np.int32
+  # np.asarray: numpy implicitly converts ndarray with one element to np.int32 (which is not
+  #   ndarray) moreover dtypes are implicitly converted to np.int64 for arithmetic operations
+
   # pad uids to uniform 7-digit length
-  uids_padded = where(uids <= 99_999, where(uids <= 99, uids * 10**5, uids * 10**2), uids)
+  uids_padded = where(uids <= 99_999,
+                      where(uids <= 99, uids * dtype(10**5), uids * dtype(10**2)),
+                      uids)
   # split uids to components (sids, iids, pids) from right to left
-  sids_iids, pids = divmod_(uids_padded, 10**2)
-  sids, iids = divmod_(sids_iids, 10**3)
-  invalid_ids = ones_like(uids) * (-1)
+  sids_iids, pids = divmod_(uids_padded, dtype(10**2))
+  if isinstance(uids, np.ndarray):
+    sids_iids = np.asarray(sids_iids, dtype=np.int32)
+  sids, iids = divmod_(sids_iids, dtype(10**3))
+  if isinstance(uids, np.ndarray):
+    sids = np.asarray(sids, dtype=np.int32)
+  invalid_ids = ones_like(uids) * dtype(-1)
   # set invalid ids
   iids = where(uids <= 99, invalid_ids, iids)
   pids = where(uids <= 99_999, invalid_ids, pids)
@@ -77,7 +100,9 @@ def decode_uids(uids,
     returns += (sids_iids,)
 
   if experimental_return_sids_pids:
-    sids_pids = sids * 10**2 + maximum(pids, 0)
+    sids_pids = sids * dtype(10**2) + maximum(pids, dtype(0))
+    if isinstance(uids, np.ndarray):
+      sids_pids = np.asarray(sids_pids, dtype=np.int32)
     returns += (sids_pids,)
 
   return returns
--- tests/scripts/utils/format_test.py
+++ tests/scripts/utils/format_test.py
@@ -1,11 +1,15 @@
 
+import sys
+assert float(sys.version[:3]) >= 3.7, 'This test uses Python >= 3.7 functionality.'
 import functools
 
 import numpy as np
 import tensorflow as tf
 assert tf.version.VERSION[0] == '2', 'This test uses TF r2.x functionality.'
 
-from scripts.utils.format import encode_ids
+from scripts.utils.format import decode_uids, encode_ids
+
+# TOTAL_TESTS = 96 + 24 = 120
 
 def encode_ids_cases():
   examples = [[(1, 2, 3), (1_002_03,)],
@@ -37,7 +41,82 @@ def encode_ids_test(cases):
     assert results == case[1][0], (case, results)
   print(f"{len(cases)} test cases completed successfully.")
 
-def 
+def decode_uids_cases():
+  ersiT = {'experimental_return_sids_iids': True}
+  erspT = {'experimental_return_sids_pids': True}
+  ersipT = {'experimental_return_sids_iids': True, 'experimental_return_sids_pids': True}
+  # examples = [example, ...]
+  # example = [inputs, outputs] = [[args, kwargs], outputs]
+  examples = [
+      [[(1,), {}], (1, -1, -1)],
+      [[(1,), ersiT], (1, -1, -1, 1)],
+      [[(1,), erspT], (1, -1, -1, 100)],
+      [[(1,), ersipT], (1, -1, -1, 1, 100)],
+      [[(11,), {}], (11, -1, -1)],
+      [[(11,), ersiT], (11, -1, -1, 11)],
+      [[(11,), erspT], (11, -1, -1, 1100)],
+      [[(11,), ersipT], (11, -1, -1, 11, 1100)],
+      [[(1_002,), {}], (1, 2, -1)],
+      [[(1_002,), ersiT], (1, 2, -1, 1002)],
+      [[(1_002,), erspT], (1, 2, -1, 100)],
+      [[(1_002,), ersipT], (1, 2, -1, 1002, 100)],
+      [[(11_002,), {}], (11, 2, -1)],
+      [[(11_002,), ersiT], (11, 2, -1, 11002)],
+      [[(11_002,), erspT], (11, 2, -1, 1100)],
+      [[(11_002,), ersipT], (11, 2, -1, 11002, 1100)],
+      [[(1_002_03,), {}], (1, 2, 3)],
+      [[(1_002_03,), ersiT], (1, 2, 3, 1002)],
+      [[(1_002_03,), erspT], (1, 2, 3, 103)],
+      [[(1_002_03,), ersipT], (1, 2, 3, 1002, 103)],
+      [[(11_002_03,), {}], (11, 2, 3)],
+      [[(11_002_03,), ersiT], (11, 2, 3, 11002)],
+      [[(11_002_03,), erspT], (11, 2, 3, 1103)],
+      [[(11_002_03,), ersipT], (11, 2, 3, 11002, 1103)],
+  ]
+
+  types_fn = [
+      functools.partial(tf.constant, dtype=tf.int32),
+      # probably np.array(..., dtype=int32) does not enforce dtype to be np.int32
+      # lambda x: np.array(x).astype(np.int32),
+      functools.partial(np.array, dtype=np.int32),
+      int,
+      np.int32
+  ]
+
+  # cases: [[inputs, outputs], ...] = [[[args, kwargs], outputs], ...]
+  cases = list()
+  for example in examples:
+    for type_fn in types_fn:
+      inputs = [(*map(type_fn, example[0][0]),), example[0][1]]
+      outputs = (*map(type_fn, example[1]),)
+      cases.append([inputs, outputs])
+  
+  return cases
+
+def decode_uids_test(cases):
+
+  def _equal(a, b):
+    # a, b: tuples of same length
+    all_equal = True
+    assert len(a) == len(b)
+    for (aa, bb) in zip(a, b):
+      assert type(aa) is type(bb), f"aa: {aa} {type(aa)}, bb: {bb} {type(bb)}"
+      if isinstance(aa, (tf.Tensor, int, np.int32)):
+        all_equal &= aa == bb
+      elif isinstance(aa, np.ndarray):
+        all_equal &= np.all(np.equal(aa, bb))
+      else:
+        raise NotImplementedError(f"{type(aa), type(bb)}")
+    return all_equal
+
+  for case in cases:
+    # print('case', case)
+    results = decode_uids(*case[0][0], **case[0][1])
+    # print(results, case[1])
+    if not _equal(results, case[1]):
+      print(case, results)
+  print(f"{len(cases)} test cases completed successfully.")
 
 if __name__ == "__main__":
+  decode_uids_test(decode_uids_cases())
   encode_ids_test(encode_ids_cases())
