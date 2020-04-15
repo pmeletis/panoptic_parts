dc349a8891b77cf41719903b933715ecb0e2eff9
pmeletis <p_c_meletis@yahoo.com>
2020-04-15 18:30:44 +0200

    Fix visualize test.

--- tests/cityscapes_panoptic_parts/experimental_visualize_test.sh
+++ tests/cityscapes_panoptic_parts/experimental_visualize_test.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-python -m cityscapes_panoptic_parts.visualize \
-  tests/test_files/cityscapes_panoptic_parts/leftImg8bit/train/aachen/aachen_000012_000019_leftImg8bit.png \
-  tests/test_files/cityscapes_panoptic_parts/gtFine/train/aachen/aachen_000012_000019_gtFine_panopticIds.tif
+python -m cityscapes_panoptic_parts.experimental_visualize \
+  tests/tests_files/cityscapes_panoptic_parts/leftImg8bit/train/aachen/aachen_000012_000019_leftImg8bit.png \
+  tests/tests_files/cityscapes_panoptic_parts/gtFine/train/aachen/aachen_000012_000019_gtFine_panopticIds.tif
