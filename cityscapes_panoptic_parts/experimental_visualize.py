a69e52d0497807b73c15a9abce58a783df25fea7
pmeletis <p_c_meletis@yahoo.com>
2020-04-17 12:02:54 +0200

    Refactor and improve docs for experimental_visualize for Cityscapes.

--- cityscapes_panoptic_parts/experimental_visualize.py
+++ cityscapes_panoptic_parts/experimental_visualize.py
@@ -1,6 +1,7 @@
 """
-Run this script as python -m cityscapes_panoptic_parts.visualize to visualize a Cityscapes
-image and label pair provided as arguments.
+Run this script as
+`python -m cityscapes_panoptic_parts.visualize <image_path> <label_path>`
+to visualize a Cityscapes image and label pair in the following 3.
 """
 
 import os.path as op
@@ -24,50 +25,27 @@ LIDS2CIDS = prob_def['lids2cids']
 LIDS2CIDS = np.array([m if m!=-1 else max(LIDS2CIDS)+1 for m in LIDS2CIDS], dtype=np.int32)
 CIDS2COLORS = np.array(prob_def['cids2colors'], dtype=np.uint8)
 
-def visualize():
+def visualize(image_path, label_path):
   """
-  Visualizes in a pyplot window iteratively images and labels from
-  image_label_output_paths and optionally writes visualizations to disk.
-  For reading and writting Pillow is used so all paths and formats
-  must be Pillow-compatible. Visualizations are written in an overwrite-safe
-  manner (only if the output path does not exist already).
-
-  Note: for fast visualizations generation during 
+  Visualizes in a pyplot window an image and a label pair from
+  provided paths. For reading Pillow is used so all paths and formats
+  must be Pillow-compatible.
 
   Args:
-    image_label_output_paths: a list of tuples:
-      (image_path, label_path) or (image_path, label_path, output_path).
+    image_path: an image path provided to Pillow.Image.open
+    label_path: a label path provided to Pillow.Image.open
   """
-  parser = argparse.ArgumentParser()
-  parser.add_argument('image_path')
-  parser.add_argument('label_path')
-  args = parser.parse_args()
-  image_path = args.image_path
-  label_path = args.label_path
   assert op.exists(image_path)
   assert op.exists(label_path)
 
   image = Image.open(image_path)
-  uids_with_lids = np.array(Image.open(label_path))
+  uids_with_lids = np.array(Image.open(label_path), dtype=np.int32)
 
-  # debugging prints
-  # print(image_path, type(uids_with_lids), uids_with_lids.dtype,
-  #       f'min: {np.min(uids_with_lids)}', f'max: {np.max(uids_with_lids)}', sep=', ')
-
-  # uids according to our panoptic format
+  # uids according to our hierarchical panoptic format
   uids_with_cids = uids_lids2uids_cids(uids_with_lids, LIDS2CIDS)
-  uids_cids_colored, uids_iids_colored, uids_pids_colored = colorize_uids(
+  uids_colored = colorize_uids(
       uids_with_cids, {cid: color for cid, color in enumerate(CIDS2COLORS)}, True)
-
-  # try:
-  #   # uids according to our panoptic format
-  #   uids_with_cids = uids_lids2uids_cids(uids_with_lids, LIDS2CIDS)
-  #   uids_cids_colored, uids_iids_colored, uids_pids_colored = colorize_uids(
-  #       uids_with_cids, {cid: color for cid, color in enumerate(CIDS2COLORS)}, True)
-  # except Exception as exc:
-  #   print(exc)
-  #   print(label_path, 'cannot be visualized.')
-  #   return False
+  uids_sem_colored, uids_sem_inst_colored, uids_sem_inst_parts_colored = uids_colored
 
   # plot
   # initialize figure for plotting
@@ -76,14 +54,18 @@ def visualize():
   #   ax.set_axis_off()
   ax1.imshow(image)
   ax1.set_title('image')
-  ax2.imshow(uids_cids_colored)
+  ax2.imshow(uids_sem_colored)
   ax2.set_title('labels colored on semantic level')
-  ax3.imshow(uids_iids_colored)
+  ax3.imshow(uids_sem_inst_colored)
   ax3.set_title('labels colored on semantic and instance levels')
-  ax4.imshow(uids_pids_colored)
+  ax4.imshow(uids_sem_inst_parts_colored)
   ax4.set_title('labels colored on semantic, instance, and parts levels')
   plt.show()
 
 
 if __name__ == "__main__":
-  visualize()
+  parser = argparse.ArgumentParser()
+  parser.add_argument('image_path')
+  parser.add_argument('label_path')
+  args = parser.parse_args()
+  visualize(args.image_path, args.label_path)
