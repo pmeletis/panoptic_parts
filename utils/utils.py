6a8667e0b29ae53b3fe4a58a6ad22f56932e3e35
Vincent Wen <x.wen@student.tue.nl>
2020-04-18 20:37:08 +0200

    Temporary update.

--- utils/utils.py
+++ utils/utils.py
@@ -99,3 +99,28 @@ def uids_lids2uids_cids(uids_with_lids, lids2cids):
                lids2cids[sids] * 10**5 + uids % 10**5))
 
   return uids_with_cids
+
+def color_map(N=256, normalized=False):
+  """ 
+  Python implementation of the color map function for the PASCAL VOC data set. 
+  Official Matlab version can be found in the PASCAL VOC devkit 
+  http://host.robots.ox.ac.uk/pascal/VOC/voc2012/index.html#devkit
+  """
+  def bitget(byteval, idx):
+      return ((byteval & (1 << idx)) != 0)
+
+  dtype = 'float32' if normalized else 'uint8'
+  cmap = np.zeros((N, 3), dtype=dtype)
+  for i in range(N):
+    r = g = b = 0
+    c = i
+    for j in range(8):
+      r = r | (bitget(c, 0) << 7-j)
+      g = g | (bitget(c, 1) << 7-j)
+      b = b | (bitget(c, 2) << 7-j)
+      c = c >> 3
+
+    cmap[i] = np.array([r, g, b])
+
+  cmap = cmap/255 if normalized else cmap
+  return cmap
