d6119c6dd9af4c8bf6c4d3eaa0785d0cc44d25cd
pmeletis <p_c_meletis@yahoo.com>
2020-04-22 16:48:03 +0200

    Fix a semantics bug in safe_write.

--- utils/utils.py
+++ utils/utils.py
@@ -73,15 +73,15 @@ def safe_write(path, image):
     image: a numpy image passed to PIL.Image.fromarray()
 
   Return:
-    True is path exists else False.
+    False is path exists. True if the `image` is successfully written.
   """
   if op.exists(path):
     print('File already exists:', path)
-    return True
+    return False
 
   os.makedirs(op.dirname(path), exist_ok=True)
   Image.fromarray(image).save(path)
-  return False
+  return True
 
 def uids_lids2uids_cids(uids_with_lids, lids2cids):
   """
