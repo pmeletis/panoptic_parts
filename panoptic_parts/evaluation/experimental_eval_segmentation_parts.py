554a9654690d990ad41ab3bc309c7198362c4deb
pmeletis <p_c_meletis@yahoo.com>
2020-11-06 22:46:44 +0100

    Simplify and refactor evaluation code.

--- panoptic_parts/evaluation/experimental_eval_segmentation_parts.py
+++ panoptic_parts/evaluation/experimental_eval_segmentation_parts.py
@@ -1,36 +1,33 @@
 """
-This script computes the confusion matrix and prints related metrics (e.g. mIoU) for pairs of
-ground truth and prediction files. The main components to use this script are the following:
+This script computes the confusion matrix and prints segmentation metrics (mAcc, mIoU) for
+ground truth and prediction files. The main components in this script are the following:
   - ground truth and prediction files for a dataset
   - an evaluation definition YAML file
   - the `ConfusionMatrixEvaluator` class from panoptic_parts.utils.experimental_evaluation
 
 The `ConfusionMatrixEvaluator` class requires:
-  1. a list of ground truth and prediction paths pairs
-  2. a reader function to read these paths and optionally convert them to a compatible format
+  1. a YAML evaluation definition filepath
+  2. a list of ground truth and prediction paths pairs
+  2. a reader function to read prediction paths and optionally convert them to a compatible format
      (see `ConfusionMatrixEvaluator` class documentation for more info)
-  3. the number of classes for initializing the confusion matrix
 
 This script generates these requirements as follows:
   1. The list of paths is created by `filepaths_pairs_fn`, change the function implementation
      according to your needs, note that `ConfusionMatrixEvaluator` requires only a list of path
      pairs, how it is created is up to the user.
-  2. The reader function reads the labels from the filepaths into tf.int32 tf.Tensor s with
+  2. The reader function reads the predictions from the filepaths into tf.int32 tf.Tensor s with
      values from the evaluation ids in `FILEPATH_EVALUATION_DEF`. We provide an example
      implementation `reader_fn`. Implement your reader function according to your
      predictions values. Definitions from `FILEPATH_EVALUATION_DEF` can be used.
-  3. The number of classes are computed using only `FILEPATH_EVALUATION_DEF`.
 
 Examples:
   1. Compute part-level class IoUs and mIoU for Cityscapes Panoptic Parts 24 parts classes:
      a. change `FILEPATH_PATTERN_GT_CPP` and `BASEPATH_PRED` and adapt the `filepaths_pairs_fn`
-        function code section denoted by ########
+        function code as needed
      b. use FILEPATH_EVALUATION_DEF = 'panoptic_parts/evaluation/defs/cpp_parts_24.yaml' and
-        adapt the `read_filepaths_and_convert` function code section denoted by ########,
-        e.g. if predictions are already encoded using the evaluation ids from
-        FILEPATH_EVALUATION_DEF, then just delete the lines in adapt section
+        adapt the `pred_reader_fn` function code as needed
      c. Run from the top-level directory panoptic_parts this script as:
-        python -m panoptic_parts.evaluation.experimental_eval_mIoU_parts
+        python -m panoptic_parts.evaluation.experimental_eval_segmentation_parts
 
 
 This is the Tensorflow CPU implementation for faster, concurrent implementation.
@@ -52,14 +49,14 @@ assert tf.version.VERSION[0] == '2', 'Uses TF r2.x functionality.'
 import yaml
 from panoptic_parts.utils.utils import _sparse_ids_mapping_to_dense_ids_mapping
 from panoptic_parts.utils.format import decode_uids
-from panoptic_parts.utils.experimental_evaluation import ConfusionMatrixEvaluator
+from panoptic_parts.utils.experimental_evaluation import (
+    ConfusionMatrixEvaluator, parse_sid_pid2eval_id)
 
 
 FILEPATH_EVALUATION_DEF = 'panoptic_parts/evaluation/defs/cpp_parts_24.yaml'
 FILEPATH_PATTERN_GT_CPP = op.join('tests', 'tests_files', 'cityscapes_panoptic_parts', 'gtFine', 'val', '*', '*.tif')
 # FILEPATH_PATTERN_GT_PPP = op.join('tests', 'tests_files', 'pascal_panoptic_parts', 'labels', '*.tif')
 BASEPATH_PRED = '/use/a/real/path'
-
 
 def filepaths_pairs_fn(filepath_pattern_gt, basepath_pred):
   # return a list of tuples with paths
@@ -80,88 +77,37 @@ def filepaths_pairs_fn(filepath_pattern_gt, basepath_pred):
 
 filepaths_pairs = filepaths_pairs_fn(FILEPATH_PATTERN_GT_CPP, BASEPATH_PRED)
 
-
-def parse_sid_pid2eval_id(sid_pid2eval_id, max_sid):
-  # this function fills in sid_pid2eval_id dictionary all missing sids_pids keys and
-  # replaces -1 values with a new eval_id, see 1., 2., 3., 4. comments for steps
-  # the returned dict contains all possible sids_pids keys, i.e., [0, (max_sid + 1) * 100]
-
-  # 1. if key -1 exists, add all non-present sids, up to max_sid, with value of sid_pid2eval_id[-1]
-  keys = sid_pid2eval_id.keys()
-  if -1 in keys:
-    for sid in range(max_sid + 1):
-      if sid not in keys:
-        sid_pid2eval_id[sid] = sid_pid2eval_id[-1]
-
-  # 2. replace sids with all sid_pid s and same values
-  # list() otherwise: RuntimeError: dictionary changed size during iteration
-  keys = list(sid_pid2eval_id.keys())
-  for key in keys:
-    if 0 <= key <= 99:
-      for pid in range(100):
-        sid_pid = key * 100 + pid
-        if sid_pid not in keys:
-          sid_pid2eval_id[sid_pid] = sid_pid2eval_id[key]
-
-  # 3. if key -1 exists, fill all not existing sid_pid s with value of sid_pid2eval_id[-1]
-  keys = sid_pid2eval_id.keys()
-  if -1 in keys:
-    for sid in range(1, max_sid + 1):
-      for pid in range(100):
-        sid_pid = key * 100 + pid
-        if sid_pid not in keys:
-          sid_pid2eval_id[sid_pid] = sid_pid2eval_id[-1]
-    del sid_pid2eval_id[-1]
-
-  # 4. replace all -1 values with id_ignore
-  id_ignore = max(sid_pid2eval_id.values()) + 1
-  sid_pid2eval_id = {k: id_ignore if v==-1 else v for k, v in sid_pid2eval_id.items()}
-
-  # TODO(panos): include assertion in _sparse_ids_mapping_to_dense_ids_mapping and make void optional
-  assert set(sid_pid2eval_id.keys()) == set(range((max_sid + 1) * 100))
-
-  return sid_pid2eval_id
-
-
-def read_filepaths_and_convert(fp_gt, fp_pred, sid_pid2eval_id):
-  # function provided to ConfusionMatrixEvaluator,
-  # see class documentation for more information
-  
-  def _py_func(fp_gt, fp_pred):
-    # eager function
-    # needed because .tif images can only be opened by Pillow for now
-    fp_gt = fp_gt.numpy().decode('utf-8')
-    label_gt = np.asarray(Image.open(fp_gt), dtype=np.int32)
-    _, _, _, sids_pids_gt = decode_uids(label_gt, return_sids_pids=True)
-    sids_pids_gt = sid_pid2eval_id[sids_pids_gt]
-    fp_pred = fp_pred.numpy().decode('utf-8')
-    label_pred = np.asarray(Image.open(fp_pred), dtype=np.int32)
-    ########################
-    # Adapt to your system #
-    # here we assume that predictions have the same format as ground truth
-    _, _, _, sids_pids_pred = decode_uids(label_pred, return_sids_pids=True)
-    sids_pids_pred = sid_pid2eval_id[sids_pids_pred]
-    assert False, 'delete this when adapted to your needs'
-    ########################
-    return sids_pids_gt, sids_pids_pred
-
-  return tf.py_function(_py_func, [fp_gt, fp_pred], (tf.int32, tf.int32))
-
-
 # preparation for evaluation
 with open(FILEPATH_EVALUATION_DEF) as fd:
   defs = yaml.load(fd)
-max_sid = defs['max_sid']
 sid_pid2eval_id = defs['sid_pid2eval_id']
+sid_pid2eval_id__values = sid_pid2eval_id.values()
+Nclasses = len(set(sid_pid2eval_id__values))
+ignore_ids = [max(sid_pid2eval_id__values) + 1] if -1 in sid_pid2eval_id__values else list()
+names = list(defs['eval_id2name'].values()) if 'eval_id2name' in defs.keys() else None
+
+#########
+# here we assume that predictions are encoded as ground truth
+def pred_reader_fn(fp_pred, sid_pid2eval_id):
+  # function provided to ConfusionMatrixEvaluator,
+  # see class documentation for more information
+  label_pred = np.asarray(Image.open(fp_pred), dtype=np.int32)
+  # here we assume that predictions have the same format as ground truth
+  _, _, _, sids_pids_pred = decode_uids(label_pred, return_sids_pids=True)
+  eids_pred = sid_pid2eval_id[sids_pids_pred]
+  return eids_pred
+
+max_sid = defs['max_sid']
 sid_pid2eval_id = parse_sid_pid2eval_id(sid_pid2eval_id, max_sid)
 sid_pid2eval_id__dense = _sparse_ids_mapping_to_dense_ids_mapping(
     sid_pid2eval_id, 0, length=(max_sid * 100 + 99) + 1)
-reader_fn = functools.partial(read_filepaths_and_convert, sid_pid2eval_id=sid_pid2eval_id__dense)
-Nclasses = len(set(sid_pid2eval_id.values()))
-ignore_ids = [max(sid_pid2eval_id.values())] if -1 in defs['sid_pid2eval_id'].values() else list()
-names = list(defs['eval_id2name'].values()) if 'eval_id2name' in defs.keys() else None
+pred_reader_fn = functools.partial(pred_reader_fn, sid_pid2eval_id=sid_pid2eval_id__dense)
+#########
 
 # create and run evaluator
-evaluator = ConfusionMatrixEvaluator(filepaths_pairs, reader_fn, Nclasses, experimental_validate_args=False)
+evaluator = ConfusionMatrixEvaluator(FILEPATH_EVALUATION_DEF,
+                                     filepaths_pairs,
+                                     pred_reader_fn,
+                                     experimental_validate_args=False)
 cm = evaluator.compute_cm()
 evaluator.print_metrics(names=names, printcmd=True, ignore_ids=ignore_ids)
--- panoptic_parts/utils/experimental_evaluation.py
+++ panoptic_parts/utils/experimental_evaluation.py
@@ -1,12 +1,70 @@
 import multiprocessing
 
+from PIL import Image
 import numpy as np
 import tensorflow as tf
 assert tf.version.VERSION[0] == '2', 'Uses TF r2.x functionality.'
+import yaml
 
-from panoptic_parts.utils.utils import _print_metrics_from_confusion_matrix
+from panoptic_parts.utils.format import decode_uids
+from panoptic_parts.utils.utils import (
+    _print_metrics_from_confusion_matrix, _sparse_ids_mapping_to_dense_ids_mapping)
 
 # VALIDATE_ARGS = True
+
+
+def parse_sid_pid2eval_id(sid_pid2eval_id, max_sid):
+  # this function fills in sid_pid2eval_id dictionary all missing sids_pids keys and
+  # replaces -1 values with a new eval_id, see 1., 2., 3., 4. comments for steps
+  # the returned dict contains all possible sids_pids keys, i.e., [0, (max_sid + 1) * 100]
+
+  # 1. if key -1 exists, add all non-present sids, up to max_sid, with value of sid_pid2eval_id[-1]
+  keys = sid_pid2eval_id.keys()
+  if -1 in keys:
+    for sid in range(max_sid + 1):
+      if sid not in keys:
+        sid_pid2eval_id[sid] = sid_pid2eval_id[-1]
+
+  # 2. replace sids with all sid_pid s and same values
+  # list() otherwise: RuntimeError: dictionary changed size during iteration
+  keys = list(sid_pid2eval_id.keys())
+  for key in keys:
+    if 0 <= key <= 99:
+      for pid in range(100):
+        sid_pid = key * 100 + pid
+        if sid_pid not in keys:
+          sid_pid2eval_id[sid_pid] = sid_pid2eval_id[key]
+
+  # 3. if key -1 exists, fill all not existing sid_pid s with value of sid_pid2eval_id[-1]
+  keys = sid_pid2eval_id.keys()
+  if -1 in keys:
+    for sid in range(1, max_sid + 1):
+      for pid in range(100):
+        sid_pid = key * 100 + pid
+        if sid_pid not in keys:
+          sid_pid2eval_id[sid_pid] = sid_pid2eval_id[-1]
+    del sid_pid2eval_id[-1]
+
+  # 4. replace all -1 values with id_ignore
+  id_ignore = max(sid_pid2eval_id.values()) + 1
+  sid_pid2eval_id = {k: id_ignore if v==-1 else v for k, v in sid_pid2eval_id.items()}
+
+  # TODO(panos): include assertion in _sparse_ids_mapping_to_dense_ids_mapping and make void optional
+  assert set(sid_pid2eval_id.keys()) == set(range((max_sid + 1) * 100))
+
+  return sid_pid2eval_id
+
+
+def _parse_yaml(fp_yaml):
+  with open(fp_yaml) as fd:
+    defs = yaml.load(fd)
+  max_sid = defs['max_sid']
+  sid_pid2eval_id = defs['sid_pid2eval_id']
+  sid_pid2eval_id = parse_sid_pid2eval_id(sid_pid2eval_id, max_sid)
+  sid_pid2eval_id__dense = _sparse_ids_mapping_to_dense_ids_mapping(
+      sid_pid2eval_id, 0, length=(max_sid * 100 + 99) + 1)
+  # Nclasses = len(set(sid_pid2eval_id.values()))
+  return sid_pid2eval_id__dense
 
 
 class ConfusionMatrixEvaluator(object):
@@ -15,27 +73,34 @@ class ConfusionMatrixEvaluator(object):
   using a tf.data pipeline for fast execution.
 
   A standard use of this class is:
-    evaluator = ConfusionMatrixEvaluator(list_of_filepaths_pairs, reader_fn, Nclasses)
+    evaluator = ConfusionMatrixEvaluator(filepath_yaml, list_of_filepaths_pairs, pred_reader_fn)
     confusion_matrix = evaluator.compute_cm()
     metrics = compute_metrics_with_any_external_function(confusion_matrix)
   """
 
-  def __init__(self, filepaths_pairs, reader_fn, Nclasses, experimental_validate_args=False):
+  def __init__(self,
+               filepath_yaml,
+               filepaths_pairs,
+               pred_reader_fn,
+               experimental_validate_args=False):
     """
     Args:
-      filepaths_pairs: [(path_gt, path_pred), ...], each pair will be passed to reader_fn
-      reader_fn: a function with inputs and outputs tf.Tensor s and signature
-        (path_gt_tensor, path_pred_tensor) -> (gt_tensor, pred_tensor),
-        gt_tensor and pred_tensor must be in range [0, Nclasses-1], i.e., the eval_ids
-      Nclasses: the number of distinct integer ids in gt_tensor and pred_tensor
+      filepath_yaml: a YAML definition from evaluation/defs
+      filepaths_pairs: [(path_gt, path_pred), ...], each path_pred will be passed to pred_reader_fn
+      pred_reader_fn: a function with a path string as input and a np.ndarray as output and
+        signature: (path_pred) -> (pred_np), pred_np must be in range [0, Nclasses-1],
+        i.e., the evaluation ids in YAML definition
       experimental_validate_args: will perform validation of tensor values to catch errors and show
         meaningful explanations, disabled by default since it is computationally intensive
     """
+    self.filepath_yaml = filepath_yaml
     self.filepaths_pairs = filepaths_pairs
-    self.filepaths_pairs_generator = lambda : (p for p in self.filepaths_pairs)
-    self.reader_fn = reader_fn
-    self.Nclasses = Nclasses
+    self.pred_reader_fn = pred_reader_fn
     self.experimental_validate_args = experimental_validate_args
+
+    self.filepaths_pairs_generator = lambda : (p for p in self.filepaths_pairs)
+    self.sid_pid2eval_id__dense = _parse_yaml(self.filepath_yaml)
+    self.Nclasses = len(np.unique(self.sid_pid2eval_id__dense))
     self.confusion_matrix = np.zeros((self.Nclasses, self.Nclasses), dtype=np.int64)
     self.num_parallel_calls = multiprocessing.cpu_count() - 1
 
@@ -66,11 +131,31 @@ class ConfusionMatrixEvaluator(object):
     new_state = old_state + cm
     return new_state
 
+  def _readers_fn(self, fp_gt, fp_pred):
+
+    def _read_gt_py(fp_gt):
+      # eager function, required since .tif images can only be opened by Pillow for now
+      fp_gt = fp_gt.numpy().decode('utf-8')
+      label_gt = np.asarray(Image.open(fp_gt), dtype=np.int32)
+      _, _, _, sids_pids_gt = decode_uids(label_gt, return_sids_pids=True)
+      eids_gt = self.sid_pid2eval_id__dense[sids_pids_gt]
+      return eids_gt
+
+    eids_gt = tf.py_function(_read_gt_py, [fp_gt], tf.int32)
+
+    def _wrapper(fp_pred):
+      fp_pred = fp_pred.numpy().decode('utf-8')
+      return self.pred_reader_fn(fp_pred)
+
+    eids_pred = tf.py_function(_wrapper, [fp_pred], tf.int32)
+
+    return eids_gt, eids_pred
+
   def compute_cm(self):
     dataset = tf.data.Dataset.from_generator(self.filepaths_pairs_generator,
                                              (tf.string, tf.string),
                                              output_shapes=((), ()))
-    dataset = dataset.map(self.reader_fn, num_parallel_calls=self.num_parallel_calls)
+    dataset = dataset.map(self._readers_fn, num_parallel_calls=self.num_parallel_calls)
     dataset = dataset.reduce(self.confusion_matrix, self._reduce_func)
     self.confusion_matrix = dataset.numpy()
     return self.confusion_matrix
